81c7e988c02714e7969b1cbc97e4bc41fcbb46c0
Saul Glasman <saulglasman0@gmail.com>
2018-06-22 14:09:17 -0400

    Getting ready to branch

--- nurikabe.hs
+++ nurikabe.hs
@@ -1,16 +1,22 @@
 import Graphics.UI.Gtk
 import Control.Exception
 import Data.Array.IO
+import Control.Monad.State
 
-type Grid = [[Either Bool Int]] -- a square in a Nurikabe grid is either a clue or an empty cell which can be shaded or unshaded
+type Square = Either Bool Int
+type Grid = [[Square]] -- a square in a Nurikabe grid is either a clue or an empty cell which can be shaded or unshaded
 
+data GridButton =  GridButton {button :: Button, --buttons will carry their coordinates around
+                               gref :: (Int, Int)}
+                               
 main :: IO ()
 main = do
        filename <- (!! 0) <$> initGUI -- get filename as command line argument
        grid <- gridFromString <$> readFile filename
-       trackingArray <- newListArray ((0, 0), (height, width)) $ concat grid
+       trackingArray <- newListArray ((0, 0), (height grid, width grid)) $ concat grid
+                        :: IO (IOArray (Int, Int) Square)
        putStrLn "Placeholder"
-
+       
 gridFromString :: String -> Grid
 -- parse a grid encoded as a list of lists of Ints. 0 denotes empty square
 gridFromString lists = assert (allEqual $ map length readLists) $ --grid must be rectangular
@@ -25,3 +31,9 @@ allEqual (x:xs) = allEqual xs && (x == head xs)
 intToSquare :: Int -> Either Bool Int
 intToSquare n = assert (n >= 0) $
                 if n == 0 then Left False else Right n
+                
+height :: Grid -> Int
+height = length
+
+width :: Grid -> Int
+width = length . (!! 0)
