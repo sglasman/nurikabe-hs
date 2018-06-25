64099cc98f8d2345ac254f2b4ed2f5c1dc4027e3
Saul Glasman <saulglasman0@gmail.com>
2018-06-25 10:45:05 -0400

    Created createElement function

--- nurikabe.hs
+++ nurikabe.hs
@@ -2,21 +2,51 @@ import Graphics.UI.Gtk
 import Control.Exception
 import Data.Array.IO
 import Control.Monad.State
+import System.Environment
 
 type Square = Either Bool Int
 type Grid = [[Square]] -- a square in a Nurikabe grid is either a clue or an empty cell which can be shaded or unshaded
+type Coord = (Int, Int)
 
 data GridButton =  GridButton {button :: Button, --buttons will carry their coordinates around
-                               gref :: (Int, Int)}
+                               gref :: Coord}
                                
 main :: IO ()
 main = do
-       filename <- (!! 0) <$> initGUI -- get filename as command line argument
+       filename <- (!! 0) <$> getArgs
        grid <- gridFromString <$> readFile filename
        trackingArray <- newListArray ((0, 0), (height grid, width grid)) $ concat grid
-                        :: IO (IOArray (Int, Int) Square)
-       putStrLn "Placeholder"
+                        :: IO (IOArray Coord Square)
+       initGUI
+       window <- windowNew
+       set window [windowTitle := "Nurikabe", containerBorderWidth := 18,
+                   windowDefaultWidth := 36 * (width grid + 1), windowDefaultHeight := 36 * (height grid + 1)]
+       table <- tableNew (height grid) (width grid) True
+       onDestroy window mainQuit
+       widgetShowAll window
+       mainGUI
        
+createElement :: (Coord, Square) -> Table -> IOArray Coord Square -> IO () -- create either a button or a label
+createElement (coord, Left _) table trackingArray = do -- create a button
+                                                    button <- buttonNew
+                                                    on button buttonActivated $ buttonClick coord trackingArray
+                                                    tableAttachAt coord table button
+
+createElement (coord, Right n) table _ = do -- create a label
+                                         label <- labelNew . Just $ show n
+                                         tableAttachAt coord table label
+                                                      
+tableAttachAt :: WidgetClass a => Coord -> Table -> a -> IO ()
+tableAttachAt (i, j) table widget = tableAttachDefaults table
+                                                        widget
+                                                        (j - 1)
+                                                        j
+                                                        (i - 1)
+                                                        i
+
+buttonClick :: Coord -> IOArray Coord Square -> IO ()
+buttonClick _ _ = putStrLn ""
+                                                        
 gridFromString :: String -> Grid
 -- parse a grid encoded as a list of lists of Ints. 0 denotes empty square
 gridFromString lists = assert (allEqual $ map length readLists) $ --grid must be rectangular
@@ -37,3 +67,6 @@ height = length
 
 width :: Grid -> Int
 width = length . (!! 0)
+
+coordArray :: Int -> Int -> [[Coord]]
+coordArray height width = map (\y -> [(y, x) | x <- [1..width]]) [1..height]
