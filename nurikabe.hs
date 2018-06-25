2d28112ede1fb85f3e48e1044c2c2fb1a4652775
Saul Glasman <saulglasman0@gmail.com>
2018-06-25 14:44:45 -0400

    Basic interface is done; just have to write the solution checker.

--- nurikabe.hs
+++ nurikabe.hs
@@ -4,35 +4,42 @@ import Data.Array.IO
 import Control.Monad.State
 import System.Environment
 
-type Square = Either Bool Int
-type Grid = [[Square]] -- a square in a Nurikabe grid is either a clue or an empty cell which can be shaded or unshaded
+data ShadeState = Unshaded | Neutral | Shaded
+type Square = Either ShadeState Clue -- a square in a Nurikabe grid is either a clue or an empty cell which can be marked shaded, unshaded or neutral
+type Clue = Int
+type Grid = [[Square]] 
 type Coord = (Int, Int)
+             
+colorWhite :: Color
+colorWhite = Color 65535 65535 65535
 
-data GridButton =  GridButton {button :: Button, --buttons will carry their coordinates around
-                               gref :: Coord}
-                               
+colorBlack :: Color
+colorBlack = Color 0 0 0
+             
 main :: IO ()
 main = do
        filename <- (!! 0) <$> getArgs
        grid <- gridFromString <$> readFile filename
-       trackingArray <- newListArray ((0, 0), (height grid, width grid)) $ concat grid
-                        :: IO (IOArray Coord Square)
+       trackingArray <- newListArray ((1, 1), (height grid, width grid)) $ concat grid :: IO (IOArray Coord Square) -- populate tracking array
+       let zippedGrid = zipWith zip (coordArray (height grid) (width grid)) grid -- zippedGrid :: [[(Coord, Square)]]
        initGUI
        window <- windowNew
        set window [windowTitle := "Nurikabe", containerBorderWidth := 18,
                    windowDefaultWidth := 36 * (width grid + 1), windowDefaultHeight := 36 * (height grid + 1)]
        table <- tableNew (height grid) (width grid) True
+       containerAdd window table
+       sequence_ . (map $ createElement table trackingArray) . concat $ zippedGrid
        onDestroy window mainQuit
        widgetShowAll window
        mainGUI
        
-createElement :: (Coord, Square) -> Table -> IOArray Coord Square -> IO () -- create either a button or a label
-createElement (coord, Left _) table trackingArray = do -- create a button
+createElement :: Table -> IOArray Coord Square -> (Coord, Square) -> IO () -- create either a button or a label
+createElement table trackingArray (coord, Left _) = do -- create a button
                                                     button <- buttonNew
-                                                    on button buttonActivated $ buttonClick coord trackingArray
+                                                    on button buttonActivated $ buttonClick button coord trackingArray
                                                     tableAttachAt coord table button
 
-createElement (coord, Right n) table _ = do -- create a label
+createElement table _ (coord, Right n) = do -- create a label
                                          label <- labelNew . Just $ show n
                                          tableAttachAt coord table label
                                                       
@@ -44,9 +51,25 @@ tableAttachAt (i, j) table widget = tableAttachDefaults table
                                                         (i - 1)
                                                         i
 
-buttonClick :: Coord -> IOArray Coord Square -> IO ()
-buttonClick _ _ = putStrLn ""
-                                                        
+buttonClick :: Button -> Coord -> IOArray Coord Square -> IO ()
+buttonClick button coord trackingArray = do 
+                                         square <- readArray trackingArray coord
+                                         case square of  -- cycle through shading states
+                                              Left Neutral -> do
+                                                              writeArray trackingArray coord $ Left Shaded
+                                                              buttonChangeColor button colorBlack
+                                              Left Shaded -> do
+                                                             writeArray trackingArray coord $ Left Unshaded
+                                                             buttonChangeColor button colorWhite
+                                                             buttonSetLabel "."
+                                              Left Unshaded -> do
+                                                               writeArray trackingArray coord $ Left Neutral
+                                                               buttonSetLabel ""
+                                                            
+buttonChangeColor :: Button -> Color -> IO ()
+buttonChangeColor button color = do widgetModifyBg button StateNormal color 
+                                    widgetModifyBg button StatePrelight color
+                                    
 gridFromString :: String -> Grid
 -- parse a grid encoded as a list of lists of Ints. 0 denotes empty square
 gridFromString lists = assert (allEqual $ map length readLists) $ --grid must be rectangular
@@ -60,7 +83,7 @@ allEqual (x:xs) = allEqual xs && (x == head xs)
 
 intToSquare :: Int -> Either Bool Int
 intToSquare n = assert (n >= 0) $
-                if n == 0 then Left False else Right n
+                if n == 0 then Left Neutral else Right n
                 
 height :: Grid -> Int
 height = length
